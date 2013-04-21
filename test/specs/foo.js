3b7447ce9ebf1bb9db7b2f3592e3e1f410c59082
Cameron Hunter <hello@cameronhunter.co.uk>
2013-04-20 20:51:05 -0700

    Adapters are now a reusable mixin

--- test/specs/foo.js
+++ test/specs/foo.js
@@ -1,17 +1,67 @@
-describeComponent('lib/flight-storage', function () {
+require(['lib/adapters'], function(Adapters) {
   
-  beforeEach(function() {
-    setupComponent();
-  });
+  var adapters = (new Adapters()).availableAdapters();
 
-  ['localstorage'].forEach(function(adapter) {
-  	describe('Adapter "' + adapter + '"', function() {
+  describeComponent('lib/flight-storage', function () {
 
-  		it("should behave in this way", function() {
-  			expect(true).toBe(true)
-  		});
+    adapters.forEach(function(adapter) {
 
-  	});
+      beforeEach(function() {
+        setupComponent({
+          adapter: adapter
+        });
+      });
+
+      describe('Adapter "' + adapter + '"', function() {
+        describe('Adapter API', function() {
+          it('should support get', function() {
+            expect(this.component.get).toBeDefined()
+          });
+
+          it('should support set', function() {
+            expect(this.component.set).toBeDefined()
+          });
+
+          it('should support clear', function() {
+            expect(this.component.clear).toBeDefined()
+          });
+        });
+
+        describe('Storage behaviour', function() {
+
+          beforeEach(function() {
+            this.component.clear();
+          });
+
+          it('should support storing simple data', function() {
+            this.component.set('foo', 'bar');
+            expect(this.component.get('foo')).toEqual('bar');
+          });
+
+          it('should support storing complex data', function() {
+            this.component.set('foo', {bar: ['baz'], boo:1331});
+            expect(this.component.get('foo')).toEqual({bar: ['baz'], boo:1331});
+          });
+
+          it('should support clearing all stored data', function() {
+            this.component.set('foo', 'bar');
+            expect(this.component.get('foo')).toEqual('bar');
+            this.component.clear();
+            expect(this.component.get('foo')).toBeUndefined();
+          });
+        });
+
+        describe('Event behaviour', function() {
+          it('should announce that data has been stored', function() {
+            var eventSpy = spyOnEvent(document, 'storageDataStored');
+            this.component.trigger('storageSet', {foo:'bar'});
+            expect(eventSpy).toHaveBeenTriggeredOn(document);
+          });
+        });
+
+      });
+
+    });
   });
 
 });
