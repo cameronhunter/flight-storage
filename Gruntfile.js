b5cdda9f2eab0104db269b0e1666f804398da4f0
Cameron Hunter <hello@cameronhunter.co.uk>
2013-04-20 19:14:54 -0700

    Adding grunt

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jasmine: {
+      test: {
+        src: 'lib/*.js',
+        options: {
+          specs: 'test/specs/**/*.js',
+          vendor: [
+            // flight dependencies
+            "components/jquery/jquery.js",
+            "components/es5-shim/es5-shim.js",
+            "components/es5-shim/es5-sham.js",
+            // jasmine extensions
+            "components/jasmine-jquery/lib/jasmine-jquery.js",
+            "components/flight-jasmine/lib/flight-jasmine.js"
+          ],
+          template: require('grunt-template-jasmine-requirejs'),
+          templateOptions: {
+            requireConfig: {
+              paths: {
+                flight: 'components/flight'
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "grunt-contrib-jasmine" task.
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jasmine']);
+
+};
