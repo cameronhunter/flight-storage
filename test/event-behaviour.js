a45667708dab8637428e908f38e035a1b7efbb5f
Cameron Hunter <hello@cameronhunter.co.uk>
2013-04-21 18:42:00 -0700

    Make the event names configurable

--- test/event-behaviour.js
+++ test/event-behaviour.js
@@ -2,6 +2,12 @@ require(['lib/adapters'], function(Adapters) {
   
   var adapters = (new Adapters()).availableAdapters();
 
+  var events = {
+    save: 'storage-save',
+    get: 'storage-get',
+    clear: 'storage-clear'
+  };
+
   describeComponent('lib/flight-storage', function () {
 
     describe('Event behaviour', function() {
@@ -9,34 +15,37 @@ require(['lib/adapters'], function(Adapters) {
 
         beforeEach(function() {
           setupComponent({
-            adapter: adapter
+            adapter: adapter,
+            saveEvent: events.save,
+            getEvent: events.get,
+            clearEvent: events.clear
           });
         });
 
         describe('Adapter "' + adapter + '"', function() {
 
           it('should announce that data has been stored', function() {
-            var eventSpy = spyOnEvent(document, 'storageDataStored');
-            this.component.trigger('storageSet', {key:'foo', value:'bar'});
+            var eventSpy = spyOnEvent(document, events.save + '-success');
+            this.component.trigger(events.save, {key:'foo', value:'bar'});
             expect(eventSpy).toHaveBeenTriggeredOn(document);
           });
 
           it('should announce that data has been retrieved', function() {
-            var eventSpy = spyOnEvent(document, 'storageDataRetrieved');
-            this.component.trigger('storageGet', {key:'foo'});
+            var eventSpy = spyOnEvent(document, events.get + '-success');
+            this.component.trigger(events.get, {key:'foo'});
             expect(eventSpy).toHaveBeenTriggeredOn(document);
           });
 
           it('should announce that data has been cleared', function() {
-            var eventSpy = spyOnEvent(document, 'storageDataCleared');
-            this.component.trigger('storageClear');
+            var eventSpy = spyOnEvent(document, events.clear + '-success');
+            this.component.trigger(events.clear);
             expect(eventSpy).toHaveBeenTriggeredOn(document);
           });
 
           it('should announce that data was not written', function() {
             spyOn(this.component, 'set').andThrow("I'm too full");
-            var eventSpy = spyOnEvent(document, 'storageDataNotStored');
-            this.component.trigger('storageSet', {key:'foo', value:'bar'});
+            var eventSpy = spyOnEvent(document, events.save + '-failure');
+            this.component.trigger(events.save, {key:'foo', value:'bar'});
             expect(eventSpy).toHaveBeenTriggeredOn(document);
           });          
 
